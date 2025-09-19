17df13ae0b64d495f2cfb89c9d5d9bea44dd590f
SatishNakka <nakkasatish619>
2025-09-19 17:17:23 +0530

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,127 +2,108 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "../features/authSlice";
+import { Form, Input } from "antd"; // Ant Design
 import logo from '../assets/Horselogo.png';
-
-<img className="logo" src={logo} alt="logo" />
-
 
 const Login = () => {
   const [form, setform] = useState({ email: "", password: "" });
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handlesubmit = (e) => {
-    e.preventDefault();
-    dispatch(login({ username: form.email.split("@")[0], email: form.email }));
+  const handlesubmit = (values) => {
+    dispatch(login({ username: values.email.split("@")[0], email: values.email }));
     navigate("/dashboard");
   };
 
   return (
     <>
-    <div className="top-title">
+      <div className="top-title">
         <h1 className="main-title">SUPPLY CHAIN </h1>
-    <h2 className="sub-title">in Plain Sight</h2>
-    </div>
-    
-    <div className="container">
-        
-      <div className="card" style={{ padding: "20px" }}>
-        <img className="logo" src={logo} alt="logo" />
-
-        <h1 className="title">PEGASUS</h1>
-        <h4 className="logistics">Logistics Group</h4>
-        <h5 className="subtitle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-right"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M5 12l14 0" />
-            <path d="M15 16l4 -4" />
-            <path d="M15 8l4 4" />
-          </svg>
-          Already have an account ?
-        </h5>
-        <h3 className="loginhere">LOGIN HERE</h3>
-        <form onSubmit={handlesubmit}>
-          <div className="div-container">
-            <input
-            className="bordercolor"
-              type="email"
-              placeholder="Email"
-              required
-              onChange={(e) => setform({ ...form, email: e.target.value })}
-            ></input>
-            <br />
-            <br />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="icon icon-tabler icons-tabler-outline icon-tabler-user"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-              <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-            </svg>
-          </div>
-
-          <div className="div-container">
-            <input
-              className="bordercolor"
-              type="password"
-              placeholder="Password"
-              required
-              onChange={(e) => setform({ ...form, password: e.target.value })}
-            ></input>
-            <br />
-            <br />
-            <svg
-              className="password"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="icon icon-tabler icons-tabler-outline icon-tabler-eye-off"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M10.585 10.587a2 2 0 0 0 2.829 2.828" />
-              <path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87" />
-              <path d="M3 3l18 18" />
-            </svg>
-          </div>
-          <div className="remember">
-            <input   className="bordercolor" type="checkbox" id="remember" name="remember" />
-            <label className="rememberme" htmlFor="remember">Remember Me <span className="forgotpassword">Forgot Password?</span></label>
-            
-          </div>
-
-          <button type="submit" className="loginbutton">
-            LOG IN
-          </button>
-        </form>
+        <h2 className="sub-title">in Plain Sight</h2>
       </div>
-    </div>
+
+      <div className="container">
+        <div className="card" style={{ padding: "20px" }}>
+          <img className="logo" src={logo} alt="logo" />
+
+          <h1 className="title">PEGASUS</h1>
+          <h4 className="logistics">Logistics Group</h4>
+          <h5 className="subtitle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-right"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 12l14 0" />
+              <path d="M15 16l4 -4" />
+              <path d="M15 8l4 4" />
+            </svg>
+            Already have an account ?
+          </h5>
+          <h3 className="loginhere">LOGIN HERE</h3>
+
+          {/* Ant Design Form */}
+          <Form
+            name="loginForm"
+            onFinish={handlesubmit}
+            layout="vertical"
+          >
+            {/* Email Field */}
+            <Form.Item
+              name="email"
+              rules={[
+                { required: true, message: "Please input your email!" },
+                { type: "email", message: "Please enter a valid email!" },
+              ]}
+            >
+              <Input
+                placeholder="Email"
+                className="bordercolor"
+                onChange={(e) => setform({ ...form, email: e.target.value })}
+              />
+            </Form.Item>
+
+            {/* Password Field */}
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: "Please input your password!" }]}
+            >
+              <Input.Password
+                placeholder="Password"
+                className="bordercolor"
+                onChange={(e) => setform({ ...form, password: e.target.value })}
+              />
+            </Form.Item>
+
+            {/* Remember Me Checkbox */}
+         <div className="remember">
+  <input
+    className="bordercolor"
+    type="checkbox"
+    id="remember"
+    name="remember"
+  />
+  <label className="rememberme" htmlFor="remember">
+    Remember Me <span className="forgotpassword">Forgot Password?</span>
+  </label>
+</div>
+
+
+            {/* Submit Button */}
+            <button type="submit" className="loginbutton">
+              LOG IN
+            </button>
+          </Form>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,9 +2,13 @@
 import React, { useState } from "react";
 import { register } from "../features/authSlice";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import logo from '../assets/Horselogo.png';
+
+// Ant Design imports
+import { Form, Input, Button, Select } from "antd";
+
+const { Option } = Select;
 
 const Register = () => {
   const [form, setForm] = useState({
@@ -24,215 +28,232 @@ const Register = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handlesubmit = (e) => {
-    e.preventDefault();
+  const handlesubmit = () => {
     dispatch(register(form));
     navigate("/dashboard");
   };
 
   return (
     <>
-    <div className="top-title">
+      <div className="top-title">
         <h1 className="main-title">SUPPLY CHAIN </h1>
-    <h2 className="sub-title">in Plain Sight</h2>
-    </div>
-      <div className="container1">
-      <div className="register-form">
-        <div className="sidelogo1">
-          <span className="registration">
-            <h1>REGISTRATION</h1>
-          </span>
-          <div>
-              <img className="logo1" src={logo} alt="logo" />
-            <h1 className="title1">PEGASUS</h1>
-            <h4 className="logistics1">Logistics Group</h4>
-          </div>
-        </div>
-        <hr />
-        <form onSubmit={handlesubmit}>
-          {/* Row 1 */}
-          <div className="form-row">
-            <div className="form-group">
-              <label>
-                &nbsp;&nbsp; Login ID <span className="star">*</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Login ID"
-                required
-                onChange={(e) =>
-                  setForm({ ...form, loginId: e.target.value })
-                }
-              />
-            </div>
-            <div className="form-group">
-              <label>
-                &nbsp;&nbsp;First Name <span className="star">*</span>
-              </label>
-              <input
-                type="text"
-                placeholder="First Name"
-                required
-                onChange={(e) =>
-                  setForm({ ...form, firstName: e.target.value })
-                }
-              />
-            </div>
-            <div className="form-group">
-              <label>
-                &nbsp;&nbsp;Last Name <span className="star">*</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Last Name"
-                required
-                onChange={(e) =>
-                  setForm({ ...form, lastName: e.target.value })
-                }
-              />
-            </div>
-          </div>
-
-          {/* Row 2 */}
-          <div className="form-row">
-            <div className="form-group">
-              <label>
-                Country <span className="star">*</span>
-              </label>
-              <select
-                onChange={(e) => setForm({ ...form, country: e.target.value })}
-              >
-                <option value="us">United States</option>
-                <option value="in">India</option>
-                <option value="uk">United Kingdom</option>
-              </select>
-            </div>
-            <div className="form-group">
-              <label>
-                &nbsp;Business Phone <span className="star">*</span>
-              </label>
-              <div className="phone-input">
-                <span>+1</span>
-                <input
-                  type="tel"
-                  required
-                  onChange={(e) =>
-                    setForm({ ...form, businessPhone: e.target.value })
-                  }
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <label>
-                Mobile Phone <span className="star">*</span>
-              </label>
-              <div className="phone-input">
-                <span>+1</span>
-                <input
-                  type="tel"
-                  required
-                  onChange={(e) =>
-                    setForm({ ...form, mobilePhone: e.target.value })
-                  }
-                />
-              </div>
-            </div>
-          </div>
-
-          {/* Row 3 */}
-          <div className="form-row">
-            <div className="form-group">
-              <label>
-                &nbsp;&nbsp;Email Address <span className="star">*</span>
-              </label>
-              <input
-                type="email"
-                placeholder="Email Address"
-                className="width"
-                required
-                onChange={(e) => setForm({ ...form, email: e.target.value })}
-              />
-            </div>
-            <div className="form-group">
-              <label>
-                &nbsp;&nbsp;Company Name <span className="star">*</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Company Name"
-                className="width"
-                required
-                onChange={(e) =>
-                  setForm({ ...form, companyName: e.target.value })
-                }
-              />
-            </div>
-          </div>
-
-          {/* Row 4 */}
-          <div className="form-row">
-            <div className="form-group">
-              <label>
-                &nbsp;&nbsp;Password <span className="star">*</span>
-              </label>
-              <input
-                type="password"
-                placeholder="Password"
-                className="width"
-                required
-                onChange={(e) =>
-                  setForm({ ...form, password: e.target.value })
-                }
-              />
-            </div>
-            <div className="form-group">
-              <label>
-                &nbsp;&nbsp;Confirm Password <span className="star">*</span>
-              </label>
-              <input
-                type="password"
-                placeholder="Confirm Password"
-                className="width"
-                required
-                onChange={(e) =>
-                  setForm({ ...form, confirmPassword: e.target.value })
-                }
-              />
-            </div>
-          </div>
-
-          {/* Captcha */}
-          <div className="form-row">
-            <div className="form-group">
-              <label>
-                &nbsp;&nbsp;Captcha <span className="star">*</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Please enter captcha"
-                className="width"
-                required
-                onChange={(e) =>
-                  setForm({ ...form, captcha: e.target.value })
-                }
-              />
-            </div>
-            <div className="captcha-box">
-              <span>L F x 4 5 Z</span>
-              <button type="button">C</button>
-            </div>
-          </div>
-
-          {/* Submit */}
-         
-          <div className="form-actions">
-              <p className="loginback">Go back to<Link to="/login"> Login</Link></p>  
-            <button type="submit" className="registerbutton">
-              Register
-            </button>
-          </div>
-        </form>
+        <h2 className="sub-title">in Plain Sight</h2>
       </div>
+      <div className="container1">
+        <div className="register-form">
+          <div className="sidelogo1">
+            <span className="registration">
+              <h1>REGISTRATION</h1>
+            </span>
+            <div>
+              <img className="logo1" src={logo} alt="logo" />
+              <h1 className="title1">PEGASUS</h1>
+              <h4 className="logistics1">Logistics Group</h4>
+            </div>
+          </div>
+          <hr />
+
+          {/* Ant Design Form */}
+          <Form
+            name="registerForm"
+            layout="vertical"
+            onFinish={handlesubmit}
+          >
+            {/* Row 1 */}
+            <div className="form-row">
+              <Form.Item
+                name="loginId"
+                rules={[{ required: true, message: "Please input your Login ID!" }]}
+              >
+                <div className="form-group">
+                  <label>&nbsp;&nbsp; Login ID <span className="star">*</span></label>
+                  <Input
+                    className="width"
+                    placeholder="Login ID"
+                    onChange={(e) => setForm({ ...form, loginId: e.target.value })}
+                  />
+                </div>
+              </Form.Item>
+
+              <Form.Item
+                name="firstName"
+                rules={[{ required: true, message: "Please input your First Name!" }]}
+              >
+                <div className="form-group">
+                  <label>&nbsp;&nbsp;First Name <span className="star">*</span></label>
+                  <Input
+                    className="width"
+                    placeholder="First Name"
+                    onChange={(e) => setForm({ ...form, firstName: e.target.value })}
+                  />
+                </div>
+              </Form.Item>
+
+              <Form.Item
+                name="lastName"
+                rules={[{ required: true, message: "Please input your Last Name!" }]}
+              >
+                <div className="form-group">
+                  <label>&nbsp;&nbsp;Last Name <span className="star">*</span></label>
+                  <Input
+                    className="width"
+                    placeholder="Last Name"
+                    onChange={(e) => setForm({ ...form, lastName: e.target.value })}
+                  />
+                </div>
+              </Form.Item>
+            </div>
+
+            {/* Row 2 */}
+            <div className="form-row">
+              <Form.Item
+                name="country"
+                rules={[{ required: true, message: "Please select your Country!" }]}
+              >
+                <div className="form-group">
+                  <label>Country <span className="star">*</span></label>
+                  <Select
+                    className="width"
+                    value={form.country}
+                    onChange={(value) => setForm({ ...form, country: value })}
+                  >
+                    <Option value="us">United States</Option>
+                    <Option value="in">India</Option>
+                    <Option value="uk">United Kingdom</Option>
+                  </Select>
+                </div>
+              </Form.Item>
+
+              <Form.Item
+                name="businessPhone"
+                rules={[{ required: true, message: "Please input your Business Phone!" }]}
+              >
+                <div className="form-group">
+                  <label>&nbsp;Business Phone <span className="star">*</span></label>
+                  <div className="phone-input">
+                    <span>+1</span>
+                    <Input
+                      className="width"
+                      type="tel"
+                      onChange={(e) => setForm({ ...form, businessPhone: e.target.value })}
+                    />
+                  </div>
+                </div>
+              </Form.Item>
+
+              <Form.Item
+                name="mobilePhone"
+                rules={[{ required: true, message: "Please input your Mobile Phone!" }]}
+              >
+                <div className="form-group">
+                  <label>Mobile Phone <span className="star">*</span></label>
+                  <div className="phone-input">
+                    <span>+1</span>
+                    <Input
+                      className="width"
+                      type="tel"
+                      onChange={(e) => setForm({ ...form, mobilePhone: e.target.value })}
+                    />
+                  </div>
+                </div>
+              </Form.Item>
+            </div>
+
+            {/* Row 3 */}
+            <div className="form-row">
+              <Form.Item
+                name="email"
+                rules={[
+                  { required: true, message: "Please input your Email!" },
+                  { type: "email", message: "Please enter a valid email!" },
+                ]}
+              >
+                <div className="form-group">
+                  <label>&nbsp;&nbsp;Email Address <span className="star">*</span></label>
+                  <Input
+                    className="width"
+                    placeholder="Email Address"
+                    onChange={(e) => setForm({ ...form, email: e.target.value })}
+                  />
+                </div>
+              </Form.Item>
+
+              <Form.Item
+                name="companyName"
+                rules={[{ required: true, message: "Please input your Company Name!" }]}
+              >
+                <div className="form-group">
+                  <label>&nbsp;&nbsp;Company Name <span className="star">*</span></label>
+                  <Input
+                    className="width"
+                    placeholder="Company Name"
+                    onChange={(e) => setForm({ ...form, companyName: e.target.value })}
+                  />
+                </div>
+              </Form.Item>
+            </div>
+
+            {/* Row 4 - Password */}
+            <div className="form-row">
+              <Form.Item
+                name="password"
+                rules={[{ required: true, message: "Please input your Password!" }]}
+              >
+                <div className="form-group">
+                  <label>&nbsp;&nbsp;Password <span className="star">*</span></label>
+                  <Input.Password
+                    className="width"
+                    placeholder="Password"
+                    onChange={(e) => setForm({ ...form, password: e.target.value })}
+                  />
+                </div>
+              </Form.Item>
+
+              <Form.Item
+                name="confirmPassword"
+                rules={[{ required: true, message: "Please confirm your Password!" }]}
+              >
+                <div className="form-group">
+                  <label>&nbsp;&nbsp;Confirm Password <span className="star">*</span></label>
+                  <Input.Password
+                    className="width"
+                    placeholder="Confirm Password"
+                    onChange={(e) => setForm({ ...form, confirmPassword: e.target.value })}
+                  />
+                </div>
+              </Form.Item>
+            </div>
+
+            {/* Captcha */}
+            <div className="form-row">
+              <Form.Item
+                name="captcha"
+                rules={[{ required: true, message: "Please enter the Captcha!" }]}
+              >
+                <div className="form-group">
+                  <label>&nbsp;&nbsp;Captcha <span className="star">*</span></label>
+                  <Input
+                    className="width"
+                    placeholder="Please enter captcha"
+                    onChange={(e) => setForm({ ...form, captcha: e.target.value })}
+                  />
+                </div>
+              </Form.Item>
+              <div className="captcha-box">
+                <span>L F x 4 5 Z</span>
+                <button type="button">C</button>
+              </div>
+            </div>
+
+            {/* Submit */}
+            <div className="form-actions">
+              <p className="loginback">Go back to <Link to="/login"> Login</Link></p>
+              <Button type="primary" htmlType="submit" className="registerbutton">
+                Register
+              </Button>
+            </div>
+          </Form>
+        </div>
       </div>
     </>
   );
